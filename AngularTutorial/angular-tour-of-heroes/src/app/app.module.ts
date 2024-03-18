56638d5ac7884ae7691edb47b4e08e5612e61603
Joao Lopes <joao.lopes@esqglobal.com>
2024-03-18 09:54:22 +0000

    Step 6 - get data from a server, using angular-in-memory-web-api

--- AngularTutorial/angular-tour-of-heroes/src/app/app.module.ts
+++ AngularTutorial/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './messages/messages.component';
 
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
-import { AppRoutingModule } from './app-routing.module';
-
 
 @NgModule({
   declarations: [
@@ -18,7 +23,8 @@ import { AppRoutingModule } from './app-routing.module';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +32,14 @@ import { AppRoutingModule } from './app-routing.module';
     FormsModule,
     NgIf,
     NgFor,
-    UpperCasePipe
+    UpperCasePipe,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
