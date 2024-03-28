026a19b6adece490a0d305789e33f355e38b4188
Joao Lopes <joao.lopes@esqglobal.com>
2024-03-28 16:27:51 +0000

    Add package-lock.json to .gitignore, unsubscribe from observable in getApiVersion().

--- client/src/app/api-version/api-version.component.ts
+++ client/src/app/api-version/api-version.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApiVersionService } from '../services/api-version.service';
 import { ApiVersion } from '../data/api-version';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-api-version',
   templateUrl: './api-version.component.html',
   styleUrls: ['./api-version.component.css']
 })
-export class ApiVersionComponent implements OnInit {
+export class ApiVersionComponent implements OnInit, OnDestroy {
   public apiVersion: ApiVersion = { 
     version: '', 
     timestamp: ''
   };
+  private unsubscribe$ = new Subject<void>();
 
   constructor(private apiVersionService: ApiVersionService) { }
 
@@ -19,8 +22,15 @@ export class ApiVersionComponent implements OnInit {
     this.getApiVersion();
   }
 
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
   getApiVersion(): void {
-    this.apiVersionService.getApiVersion().subscribe( {
+    this.apiVersionService.getApiVersion()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
         next: (apiVersion: ApiVersion) => {this.apiVersion = apiVersion},
         error: (error: any) => {console.log(error)}
       });
